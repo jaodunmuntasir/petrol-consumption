38e54a1edcf83a3f692fef0d5b095d2d78738f66
Jaodun Muntasir <126531434+jaodunmuntasir@users.noreply.github.com>
2023-02-28 02:24:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Versioning;
+
+namespace program4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Amount of petrol consumed (liters): ");
+            double petrol = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Length of the trip (km): ");
+            double travel = Convert.ToDouble(Console.ReadLine());
+
+            double consumption = Convert.ToDouble((petrol / (travel / 100)));
+
+            Console.WriteLine("The consumption of the car is " + consumption + " (liters / 100 km).");
+        }
+    }
+}
